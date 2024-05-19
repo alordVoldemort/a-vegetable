418f124c587f3da57bd44b033a50a052d4035382
AvinashS <avinash.suryawanshi1@mphasis.com>
2024-05-19 14:09:12 +0530

    added task

---
+++ task.sh
@@ -0,0 +1,38 @@
+1. add botton 'create entry' open model with below  fields
+    1. input -enter vegitable name
+    2.  from city - to city  - dropdown = [ 'nashik', 'pune' ]
+    3. driver name  - dropdown = { name: "aniket' , email, phone, alt phone, address }
+    5. client name  - dropdown  = { name: "aniket' , email, phone, alt phone, address }
+    8  client total amount - dropdown - input
+    9. advance - - input
+    9. vegitable waight - input 
+    9.1 = kg/tun 
+    10. photo as attachment  - type file
+    11. note - textarea 
+1.1 : list table 
+
+2. cusstom entry : 
+    add city - - { name  }
+    add client  - - { name , email, phone, alt phone, address, }
+    add driver - { name , email, phone, alt phone, address, }
+
+3. create user 
+    - name
+    - email 
+    - phone
+    - photo
+    - password
+
+4. edit profile 
+    - name
+    - email 
+    - phone
+    - photo
+    - password
+
+5. dashboard : 
+   - total amount
+   - entry 
+   - pending 
+   - advance  
+   - 
