41097df407e15a4c487f2e9b00c5976d9953a032
Aniket Sangale <aniketsangale23@gmail.com>
2024-06-16 23:34:01 +0530

    db add

--- server/bin/db.js
+++ server/bin/db.js
@@ -3,11 +3,18 @@ const mysql = require('mysql');
 
 // Configure MySQL connection
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'vegetable-app'
+    host: 'byfpzhdvlz7zqudwubbi-mysql.services.clever-cloud.com',
+    user: 'ug78ruyy7wimxelp',
+    password: 'AeFnIDcylyWPBUOGh65D',
+    database: 'byfpzhdvlz7zqudwubbi',
+    port: 3306
 });
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'vegetable-app'
+// });
 
 connection.connect(err => {
   if (err) {
