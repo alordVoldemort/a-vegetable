8db9b21c6c7792ad2d7feaf15ec32c7e9e85d5f5
alordVoldemort <142912226+alordVoldemort@users.noreply.github.com>
2024-06-16 22:51:36 +0530

    Update app.js
    cors

--- server/app.js
+++ server/app.js
@@ -2,12 +2,18 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cors = require('cors');
 
+var logger = require('morgan');
+app.options('*', cors());
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(cors({
+  origin: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.options('*', cors());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
